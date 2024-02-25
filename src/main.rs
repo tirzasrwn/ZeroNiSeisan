5151bc0e83a2c39e6543785aff4d46ec68e4bcb4
tirzasrwn <tirzasrwn@gmail.com>
2024-02-25 23:04:23 +0700

    feat: Implement first integration test.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use ::zero2seisan::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000.");
+    run(listener)?.await
 }
 
